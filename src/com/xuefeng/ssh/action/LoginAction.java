892b129681738a75404599be6044d3d935f87908
fxtxiaozhu <zyh@192.168.253.1>
2016-08-19 14:25:36 +0800

    sdfssdfsdf

---
+++ src/com/xuefeng/ssh/action/LoginAction.java
@@ -0,0 +1,52 @@
+package com.xuefeng.ssh.action;
+
+import com.opensymphony.xwork2.Action;
+import com.xuefeng.ssh.service.LoginService;
+
+public class LoginAction implements Action {
+
+	// 对应于login.jsp的输入，�?��getter和setter
+	private String username;
+
+	private String password;
+
+	/**
+	 * 验证登录的�?�?
+	 */
+	private LoginService loginService;
+	
+	public LoginAction() {
+//		loginService = new LoginServiceImpl();
+	}
+
+	public String execute() throws Exception {
+		if (loginService != null && loginService.validate(username, password)) {
+			return SUCCESS;
+		}
+		return INPUT;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LoginService getLoginService() {
+		return loginService;
+	}
+
+	public void setLoginService(LoginService loginService) {
+		this.loginService = loginService;
+	}
+}
---
+++ src/com/xuefeng/ssh/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.xuefeng.ssh.dao;
+
+public interface UserDao {
+	public boolean validate(String username, String password);
+}
